1cf56f7c629a71e7a414dce78fecc30d1a4c5813
Blazej <nitka.blazej@gmail.com>
2020-04-09 20:22:50 +0200

    First initialize commit

---
+++ src/main/java/utp/nitpac/Project.java
@@ -0,0 +1,15 @@
+package utp.nitpac;
+
+/**
+ * Główna klasa projektu.
+ */
+public class Project {
+
+    public static void main(String[] args) {
+        System.out.println(String.format("Java version: %s", System.getProperty("java.version")));
+        System.out.println(String.format("Java runtime version: %s", System.getProperty("java.runtime.version")));
+    }
+
+}
+
+
