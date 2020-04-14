f246c2a780ace20ae53fce5465ce7db7dfdba5e3
nitkablazej <nitka.blazej@gmail.com>
2020-04-14 19:51:14 +0200

    Update Project.java
    Poprawa nazwy przedmiotu

--- src/main/java/utp/nitpac/Project.java
+++ src/main/java/utp/nitpac/Project.java
@@ -8,7 +8,7 @@ public class Project {
     public static void main(String[] args) {
         System.out.println(String.format("Java version: %s", System.getProperty("java.version")));
         System.out.println(String.format("Java runtime version: %s", System.getProperty("java.runtime.version")));
-        System.out.println("Project name: ALGORYTMY I STRUKTURY DANYCH");
+        System.out.println("Project name: ALGORYTMY I EKSPLORACJA DANYCH");
         System.out.println("DEV1: " + DEV1.author);
         System.out.println("DEV2: " + DEV2.author);
     }
